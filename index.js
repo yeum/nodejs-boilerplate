a41b057c857f8f4e85b5b0755401523b1e877ca4
YUM <yum@YUMui-MacBookPro.local>
2020-08-16 00:19:10 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
+const {User} = require('./models/User')
+
+// client에서 오는 정보를 server에서 분석해서 가져올 수 있게함
+// application/x-www-form-urlencoded 를 분석해서 가져올 수 있게함
+app.use(bodyParser.urlencoded({extended: true}))
+// application/json 타입을 분석해서 가져올 수 있게힘
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://yum:duadua129@boilerplate.amuu8.mongodb.net/boilerplate?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
+app.get('/', (req, res) => res.send('Hello World!'))
 
-
-app.get('/', (req, res) => res.send('Hello World!~ 안녕하세요~'))
+app.post('/register', (req, res) => {
+    // 회원가입 할 때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        // status(200)은 성공했다는 표시
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log('Example app listening on port ${port}!'))
